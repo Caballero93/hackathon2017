691faeaf37d073d0be2a738c6972297420b1691c
Novak Boškov <novak.boskov@typhoon-hil.ch>
2017-10-24 15:49:13 +0200

    Visualization refresh rate control

--- viz/viz.js
+++ viz/viz.js
@@ -23,12 +23,44 @@ function getResultsJSON() {
     });
 }
 
-var results = c3.generate({
-    bindto: '#results',
-    data: {
-        columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ]
+/**
+ * Set page refresh rate from refresh rate input if set or stop refreshing.
+ * @param {Bool} stop - stop refreshing
+ * @param {Bool} set - set refresh rate using refresh rate input field
+ */
+function setPageRefresh(stop=false, set=false) {
+    qRefresh = window.location.search.split('=')[1];
+    if (qRefresh === undefined) {
+        qRefresh = 1;
     }
-});
+
+    metaRefresh = $('head')
+        .append('<meta http-equiv="refresh" content="' + qRefresh + '">');
+
+    if (stop) {
+        window.location.search = 'refreshRate=false';
+    } else if (set) {
+        window.location.search = 'refreshRate=' + $('#refreshRate').val();
+    }
+}
+
+function vizResults() {
+    var results = c3.generate({
+        bindto: '#results',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
+        }
+    });
+}
+
+/**
+ * Runs on <body onload>
+ */
+function vizOnLoad() {
+    setPageRefresh();
+    $('#refreshRate').val(parseInt(window.location.search.split('=')[1]));
+    vizResults();
+}
