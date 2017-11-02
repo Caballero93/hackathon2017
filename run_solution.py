e75480d7d38b96a40c7e71a7c4125f2ee0b4b83a
Novak Boškov <novak.boskov@typhoon-hil.ch>
2017-11-02 15:11:53 +0100

    Run script

--- run_solution.py
+++ run_solution.py
@@ -16,9 +16,9 @@ __license__ = "MIT"
 
 def worker(msg: DataMessage) -> ResultsMessage:
     """TODO: This function should be implemented by contestors."""
-    print('D: received {}'.format(data.id))
+    print('D: received {}'.format(msg.id))
     print('Worker doing its job, message is {} ...'.format(msg))
-    print('D: is going to send {}'.format(data.id))
+    print('D: is going to send {}'.format(msg.id))
 
     return ResultsMessage(data_msg=msg,
                           load_one=True,
@@ -27,8 +27,12 @@ def worker(msg: DataMessage) -> ResultsMessage:
                           power_reference=0.0,
                           pv_mode=PVMode.ON)
 
-if __name__ == '__main__':
+def run():
     cntrl = Control()
 
     for data in cntrl.get_data():
         cntrl.push_results(worker(data))
+
+
+if __name__ == '__main__':
+    run()
