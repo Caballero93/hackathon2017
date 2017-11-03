71b0dc722e97cd8f0535ba8a99920830b4496a7a
Predrag Nikolic <predrag@typhoon-hil.com>
2017-11-03 17:28:19 +0100

    Changed visualizators

--- visualize_results.py
+++ visualize_results.py
@@ -52,26 +52,34 @@ time_span = len(overall) / CFG.sampleRate
 t = np.arange(0., time_span, 1./CFG.sampleRate)
 
 fig, ax = plt.subplots(3, sharex=True)
+
 ax[0].step(t, overall, picker=True)
 ax[0].step(t, overall_energy)
 ax[0].step(t, overall_penalty)
 ax[0].step(t, overall_performance)
 ax[0].set_title('Results')
 ax[0].legend(['Overall cost', 'Energy cost', 'Penalty cost', 'Computational cost'], loc = 'upper right', fontsize = 'small')
+ax[0].grid(True, linestyle='--')
+
 ax[1].step(t, grid_status)
 ax[1].step(t, bessSOC)
 ax[1].step(t, bessOverload)
 ax[1].legend(['Grid status', 'BESS SOC', 'BESS overload'], loc = 'upper right', fontsize = 'small')
+ax[1].grid(True, linestyle='--')
+
 ax[2].step(t, bessPower)
 ax[2].step(t, mainGridPower)
 ax[2].step(t, real_load)
 ax[2].step(t, pv_power)
 ax[2].legend(['BESS power', 'Main grid power', 'Total load', 'PV power'], loc = 'upper right', fontsize = 'small')
+ax[2].grid(True, linestyle='--')
+
 
 plt.xlim(0, time_span)
 
 formatter = matplotlib.ticker.FuncFormatter(lambda m, x: time.strftime('%H:%M', time.gmtime(m*60*60)))
 ax[2].xaxis.set_major_formatter(formatter)
 
+plt.tight_layout(pad=0.2, h_pad=1.0)
 
 plt.show()
