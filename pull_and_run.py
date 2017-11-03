67af2910e50a42bc38b95a9996c5c96e176ed4af
Novak Boškov <novak.boskov@typhoon-hil.ch>
2017-11-03 20:00:35 +0100

    Pull and run script

---
+++ pull_and_run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import os
+import subprocess
+import shutil
+
+if __name__ == '__main__':
+    theirs = 'hackathon2017.their'
+    subprocess.run(['git', 'clone', sys.argv[1], theirs])
+
+    # Remove ours requirements file
+    os.remove('requirements.txt')
+    # Get theirs
+    shutil.copyfile(os.path.join(theirs, 'requirements.txt'),
+                    'requirements.txt')
+
+    # Remove our hackathon/solution
+    shutil.rmtree(os.path.join('hackathon', 'solution'))
+    # Get their hackathon/solution
+    shutil.copytree(os.path.join(theirs, 'hackathon', 'solution'),
+                    os.path.join('hackathon', 'solution'))
+
+    # Remove their repository completely
+    shutil.rmtree(theirs)
+
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+
+    subprocess.run(['python', 'run.py'])
