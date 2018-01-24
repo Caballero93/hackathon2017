63156dd8e1bb4bd6967b84b768348e18ef25f26b
Stefan <stefan.soic@typhoon-hil.ch>
2018-01-24 15:08:58 +0100

    Ispravljena kontrola solara tokom off-grid perioda

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -6,7 +6,8 @@ from hackathon.utils.utils import ResultsMessage, DataMessage, PVMode, \
 from hackathon.framework.http_server import prepare_dot_dir
 from pulp import *
 from hackathon.energy.rating import PENAL_L1_INIT, PENAL_L2_INIT, PENAL_L1_CONT, PENAL_L2_CONT, PENAL_L3_CONT
-
+global flag_solar
+flag_solar = False
 
 def worker(msg: DataMessage) -> ResultsMessage:
     """TODO: This function should be implemented by contestants."""
@@ -21,6 +22,7 @@ def worker(msg: DataMessage) -> ResultsMessage:
     global L1_old
     global L2_old
     global L3_old
+    global flag_solar
 
     # initialize control variables
     L1, L2, L3 = True, True, True
@@ -40,8 +42,12 @@ def worker(msg: DataMessage) -> ResultsMessage:
         if msg.bessSOC < 0.2:
             L2, L3 = False, False
         if msg.solar_production > msg.current_load and msg.bessSOC > 0.99:
+            flag_solar = True
+            #panel = PVMode.OFF
+        if flag_solar:
             panel = PVMode.OFF
     else:
+        flag_solar = False
         if msg.buying_price == 3:
             if msg.bessSOC != 1:
                 p_bat = -6.0
@@ -86,14 +92,13 @@ def worker(msg: DataMessage) -> ResultsMessage:
                 elif v.name == 'MILP_L3':
                     L3 = bool(v.varValue)
 
-            real_load = L1 * LOAD_1 + L2 * LOAD_2 + L3 * LOAD_3
-            temp = P_pv - real_load
-            if msg.selling_price == 0:
-                if temp > 0:
-                    p_bat = - temp if msg.bessSOC != 1 else 0.0
+        real_load = L1 * LOAD_1 + L2 * LOAD_2 + L3 * LOAD_3
+        temp = P_pv - real_load
+        if temp > 0:
+            p_bat = - temp if msg.bessSOC != 1 else 0.0
 
-            if (p_bat + P_pv) > real_load and temp <= 0:
-                p_bat = real_load - P_pv
+        if (p_bat + P_pv) > real_load and temp <= 0:
+             p_bat = real_load - P_pv
 
 
     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
